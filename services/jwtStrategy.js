fa237052987db75ed628b9de529908a1632b8438
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-27 12:05:35 +0800

    Create JWT Strategy

---
+++ services/jwtStrategy.js
@@ -0,0 +1,25 @@
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const jwtStrategy = passportJWT.Strategy;
+const ExtractJwt = passportJWT.ExtractJwt;
+
+const User = require('../models/User');
+const dotenv = require('dotenv').config();
+
+const jwtLogin = new jwtStrategy({
+    jwtFromRequest: ExtractJwt.fromHeader('token'),
+    secretOrKey: process.env.JWT_SECRET
+}, async(payload, done) => {
+    try {
+        const user = await User.find({email_address: payload.email});
+        if (user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    } catch(err) {
+        done(err);
+    }
+});
+
+passport.use(jwtLogin);
