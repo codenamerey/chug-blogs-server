48fde9e9d9e87fff80a1c2e40aa45cf4e24b64b3
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-27 13:18:58 +0800

    Add sign up functionality to local strategy

--- services/localStrategy.js
+++ services/localStrategy.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcrypt');
 const localLogin = new LocalStrategy(
         async(email, password, done) => {
             try{
-                const user = await User.find({email_address: email});
+                const user = await User.find({email_address: email})[0];
                 const isMatch = await bcrypt.compare(password, user.password);
                 if(!isMatch) {
                     done(null, false, {message: 'User not found'});
@@ -21,3 +21,31 @@ const localLogin = new LocalStrategy(
 );
 
 passport.use(localLogin);
+
+const LocalSignUp = new LocalStrategy({
+    passReqToCallback: true
+}, async(email, password, done) => {
+    try{
+        const user = await User.find({email_address: email})[0];
+        if(user) {
+            done(null, false, {message: 'User already exists'});
+        } else {
+            const {first_name, last_name, display_picture} = req.body;
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const newUser = new User({
+                first_name,
+                last_name,
+                display_picture,
+                password: hashedPassword,
+                email_address: email
+            });
+
+            await newUser.save();
+            done(null, newUser);
+        }
+    } catch(err) {
+        done(err);
+    }
+})
+
+passport.use('local-signup', LocalSignUp);
