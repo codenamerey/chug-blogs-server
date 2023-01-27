457d6b929e73eec4c299880ce4d1771346222ba7
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-27 16:48:29 +0800

    Get only found user by email address

--- services/localStrategy.js
+++ services/localStrategy.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcrypt');
 const localLogin = new LocalStrategy(
         async(email, password, done) => {
             try{
-                const user = await User.find({email_address: email});
+                const user = await User.find({email_address: email})[0];
                 const isMatch = await bcrypt.compare(password, user.password);
                 if(!isMatch) {
                     done(null, false, {message: 'User not found'});
