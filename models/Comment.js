97415c1b67fc7dcf499c8cd7ef892a232f24fe74
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-27 10:25:47 +0800

    Create comment model

---
+++ models/Comment.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+    author: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Post', PostSchema);
