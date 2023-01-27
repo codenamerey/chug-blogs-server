c0ca3bbb20fcc4162835d26a710702752afd2677
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-27 10:13:32 +0800

    Create Post model

---
+++ models/Post.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    description: {
+        type: String
+    },
+    slug: {
+        type: String,
+        required: true
+    },
+    post_image: {
+        type: String
+    },
+    private: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('Post', PostSchema);
