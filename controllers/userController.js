308b614f5159379f4446c6ed31d17884e7fe0c8b
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-28 10:38:19 +0800

    Allow user sign in

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,9 @@
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const dotenv = require('dotenv').config();
 
 // Local Strategy
 exports.local_sign_up = async(req, res, next) => {
@@ -26,4 +28,19 @@ exports.local_sign_up = async(req, res, next) => {
         res.status(500).json({err: err.message});
     }
         
+}
+
+exports.local_sign_in = (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if(err) return next(err);
+        if(user) {
+            const token = user.generateJWTToken();
+            res.status(200).json({message: 'Logged in', token});
+        } else {
+            console.log(user);
+            console.log(req.body);
+            res.status(422).json({message: 'Invalid credentials'});
+        }
+        
+    })(req, res, next)
 }
--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,6 @@ const userController = require('../controllers/userController');
 
 // Local Strategy Auth
 router.post('/local/signup', userController.local_sign_up);
+router.post('/local/signin', userController.local_sign_in);
 
 module.exports = router;
