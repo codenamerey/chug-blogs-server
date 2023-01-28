08ef1db2c06dc1859371b3cd6214c5ed3cc21e57
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-28 10:38:08 +0800

    Adjust application to accept req body

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT;
 const MONGODB = process.env.MONGODB;
 const DOMAIN = process.env.DOMAIN;
 
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(passport.initialize());
+require('./services/localStrategy');
 
 // Connect to DB
 mongoose.connect(MONGODB, {
@@ -20,9 +27,6 @@ DB.on('error', console.error.bind(console, 'MongoDB Connection Error: '));
 const user = require('./routes/user');
 const comment = require('./routes/comment');
 const post = require('./routes/post');
-
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
 
 app.use('/api/user', user);
 // app.use('/api/comment', comment);
