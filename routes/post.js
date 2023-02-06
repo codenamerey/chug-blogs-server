5aa0d86b230eda0ef7ed3e75a04b1bb7a2969a97
Lee Ryan <2solimanleeryan@gmail.com>
2023-02-06 12:03:49 +0800

    Allow deletion of blog post

--- routes/post.js
+++ routes/post.js
@@ -5,7 +5,8 @@ const {
    post_create,
    post_get_all,
    post_get_one,
-   post_edit 
+   post_edit,
+   post_delete 
 } = require('../controllers/postController');
 
 router.get('/', post_get_all);
@@ -13,5 +14,6 @@ router.post('/', post_create);
 
 router.get('/:id', post_get_one);
 router.put('/:id', post_edit);
+router.delete('/:id', post_delete);
 
 module.exports = router;
